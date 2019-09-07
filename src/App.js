6e8d7834a2a459fea3fd4fdd2881a348a3f725e1
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-09-07 19:37:36 +0530

    bootstrap navbar basic added

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import {
 } from 'react-router-dom';
 
 import { Button } from 'react-bootstrap';
-
+import Navigation from './components/Navigation';
+import About from './About';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navigation />
         <h1>App page</h1>
         <Button variant="primary">Primary</Button>
+        <Route path='' component={About} />
       </div>
     </Router>
   );
