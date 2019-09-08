009425427fbf1f011331fab8165ed53c8b205dd7
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-09-08 18:55:11 +0530

    todo page added

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,27 +1,29 @@
 import React, { useState, useEffect } from 'react';
 
-export default function Todo() {
+export default function Todo({ match }) {
 
     useEffect(() => {
         fetchTodo();
+        console.log({ match });
     }, []);
 
     const [todo, setTodo] = useState([]);
 
     const fetchTodo = async () => {
 
-        const data = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const data = await fetch(`https://jsonplaceholder.typicode.com/todos/${match.params.id}`);
 
         const todo = await data.json();
 
         console.log({ todo });
+
         setTodo(todo);
     };
 
     return (
         <div>
             <h1>Todo page</h1>
-            <p>{todo.id}</p>
+            <p>{todo.id} - {todo.title}</p>
         </div>
     )
 }
