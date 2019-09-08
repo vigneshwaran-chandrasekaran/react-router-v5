d4ef494bc465cee44f3a355ebd29cd62ecb96752
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-09-08 18:55:46 +0530

    about page back to original state

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,32 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 
 export default function About() {
-
-    useEffect(() => {
-        fetchItems();
-    }, []);
-
-    const [todos, setTodos] = useState([]);
-
-    const fetchItems = async () => {
-
-        const data = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10');
-
-        const todos = await data.json();
-
-        console.log({ todos });
-        setTodos(todos);
-    };
-
     return (
         <div>
             <h1>About page</h1>
-            {todos.map(todo => (
-                <p key={todo.id}>
-                    <Link to={`/todo/${todo.id}`}>{todo.id} - {todo.title}</Link>
-                </p>
-            ))}
         </div>
     )
 }
