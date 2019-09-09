16830a31656b0fb947788a424cad2ed7ad9d0433
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-09-09 11:46:33 +0530

    basic prompt added in contact page, if textbox value entered then user click to go new route prompt will invoke

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Prompt } from 'react-router'
 
 function Contact() {
+  const [name, setName] = useState('');
+
+  function handleOnChange(e) {
+    setName(e.target.value);
+  }
   return (
     <div>
+      <Prompt
+        when={!!name}
+        message="Are you sure you want to leave this page?"
+      />
       <h1>Contact page</h1>
+      <input
+        value={name}
+        onChange={handleOnChange}
+        placeholder='Name' />
+      <p>{name}</p>
     </div>
   )
 }
