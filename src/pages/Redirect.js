cb51bbe0d08f47b3244d2be3e9934b5f6e764366
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-09-16 20:44:16 +0530

    Redirect code simplified

--- src/pages/Redirect.js
+++ src/pages/Redirect.js
@@ -3,23 +3,16 @@ import { Redirect } from 'react-router';
 
 export default function RedirectPage() {
     const [state, setState] = useState(0);
+
     function handleOnClick() {
         setState(state + 1);
-    }
-
-    function checkRedirect() {
-        console.log(state < 5);
-        return state < 5;
     }
 
     return (
         <div>
             <p>On fifth time button click page will direct to dashboard Page using "Redirect"</p>
-            {checkRedirect() ?
-                <button className='btn btn-warning' onClick={handleOnClick}>Click me {state}</button> :
-                <Redirect to="/dashboard" />
-            }
-
+            {(state >= 5) && <Redirect to="/dashboard" />}
+            <button className='btn btn-warning' onClick={handleOnClick}>Click me {state}</button>
         </div>
     )
 }
