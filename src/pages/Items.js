f6c26e9c26dffb9d69a77a272d96436b9ce5248f
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-11-12 23:02:01 +0530

    on input change update the field value to url

--- src/pages/Items.js
+++ src/pages/Items.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default function Items({ data }) {
-  console.log(data);
+export default function Items(props) {
+  const [state, setState] = useState("");
+
+  useEffect(() => {
+    console.log({ props });
+    // const userName = props.location.search
+  }, []);
+
+  function handleChange(event) {
+    const data = event.target.value;
+    setState(data);
+    props.history.push({
+      search: `?userName=${data}`
+    });
+  }
+
+  function handleSubmit(event) {
+    console.log("handleSubmit");
+    event.preventDefault();
+  }
   return (
     <div>
-      <p>Items ww {data ? data.category : ""}</p>
+      <p>Items ww {props.data ? props.data.category : ""}</p>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="userName"
+            value={state}
+            onChange={handleChange}
+          />
+        </label>
+        <div>
+          <input className="btn btn-success" type="submit" value="Submit" />
+        </div>
+      </form>
     </div>
   );
 }
