2e97a772fc6f814fabac8a85e0dfe84f97128c83
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-11-12 23:17:09 +0530

    added qs package for query string handling

--- src/pages/Items.js
+++ src/pages/Items.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from "react";
+import qs from "qs";
 
 export default function Items(props) {
   const [state, setState] = useState("");
 
   useEffect(() => {
     console.log({ props });
-    // const userName = props.location.search
+    const urlParams = qs.parse(props.location.search, {
+      ignoreQueryPrefix: true // will ignore the '?'
+    });
+    console.log({ urlParams });
+    if (urlParams.userName) {
+      setState(urlParams.userName);
+    }
   }, []);
 
   function handleChange(event) {
@@ -20,10 +27,10 @@ export default function Items(props) {
     console.log("handleSubmit");
     event.preventDefault();
   }
+
   return (
     <div>
       <p>Items ww {props.data ? props.data.category : ""}</p>
-
       <form onSubmit={handleSubmit}>
         <label>
           Name:
