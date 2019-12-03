9398d7179b4339b9cfd52a11380114096b3abdc1
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-12-03 19:57:16 +0530

    for deployment added npm script commmand

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,9 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <h1>Home pages</h1>
+      <h1>Home pages starts</h1>
       <p>{process.env.REACT_APP_NAME}</p>
+      <h1>Home pages ends</h1>
     </div>
   );
 }
