daab7d487957513c6d1ef52e2966bfbf587b0605
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-12-03 19:24:44 +0530

    test changed added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <h1>Home page</h1>
+      <h1>Home pages</h1>
       <p>{process.env.REACT_APP_NAME}</p>
     </div>
   );
